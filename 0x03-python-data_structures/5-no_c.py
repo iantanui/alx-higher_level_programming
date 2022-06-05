d6e7ac2d3c79b45fa4565e83e7c75bef5b7efab5
Ian Tanui <72918706+iantanui@users.noreply.github.com>
2022-06-05 20:56:48 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    return my_string.translate({ord(c):None for c in "cC"})
+    s = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            s += char
+
+    return s
