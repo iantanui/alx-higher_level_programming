386ca587f44bf533421b52114fdd57f214d76091
iantanui <rutooian@gmail.com>
2022-07-09 14:34:11 -0700

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+8-class_to_json module
+"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary descriotion with simple data structure for
+    JSON serialization of an object
